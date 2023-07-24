951c76101fa33f4b7f57a7ccd3dfc6800d89eb96
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-07-24 15:50:46 +0530

    on the Header.css Part

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial'
+
+
+
 const Header = () => {
   return (
     <header>
@@ -10,7 +15,13 @@ const Header = () => {
 
         <h5 className="text-light">FrontEnd Web Developer</h5>
         <CTA />
+        <HeaderSocial />
+        <div className="me"></div>
+       <img src={ME} alt="me"  />
       </div>
+
+      <a href="#contact" className='scroll-down'>Scroll Down</a>
+
     </header>
   )
 }
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+const HeaderSocial = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com/in/itsmesuraj20" target='_blank'><BsLinkedin/></a>
+        <a href="https://github.com/itsmesuraj20" target='_blank'><FaGithub/></a>
+        
+        
+        </div>
+  )
+}
+
+export default HeaderSocial
